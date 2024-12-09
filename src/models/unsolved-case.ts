6a42e17d53b728b3dac738acd930d6e52ef3436d
Ewelina <ewelina.petelicka01@gmail.com>
2024-12-09 16:54:48 +0100

    create unsolved case model & add data to db

---
+++ src/models/unsolved-case.ts
@@ -0,0 +1,10 @@
+export interface UnsolvedCase {
+    "caseName": string,
+    "date": string,
+    "location": string,
+    "description": string,
+    "lastKnownActivity": string,
+    "keyEvidence": string[]
+    "suspects": string[],
+    "notes": string
+}
